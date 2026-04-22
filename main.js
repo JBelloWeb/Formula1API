12cba5150db275aba47bc40cdddfdbcd0ecdca37
JBelloWeb <juanperalbello@gmail.com>
2026-04-22 16:29:03 -0300

    favs admin

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ const filter = d.querySelector('.teams');
 const container = d.querySelector('.container');
 const scuderias = [];
 const drivers = [];
+const storedFavourites = localStorage.getItem('driversFavoritos');
+const favourites = storedFavourites ? JSON.parse(storedFavourites) :  [];
 const year = d.querySelector('#yearSelector');
 const race = d.querySelector('#raceSelector');
 
@@ -109,54 +111,85 @@ const getChampionshipInfo = async() =>{
 }
 getChampionshipInfo();
 
+const inArray = (array, name) =>{
+    return array.includes(name);
+}
+
+const defineStyle = (name) =>{
+    let favs = JSON.parse(localStorage.getItem('driversFavoritos')) || [];
+    return `<i class=" ${inArray(favs, name) ? 'fa-solid' : 'fa-regular'} fa-star"></i>`;
+}
+
+const gestionarFavoritos = (nombre) =>{
+    let favs = JSON.parse(localStorage.getItem('driversFavoritos')) || [];
+    inArray(favs, nombre) ? favs.splice(i => {favs.indexOf(nombre)}, 1) : favs.push(nombre);
+
+    localStorage.setItem('driversFavoritos', JSON.stringify(favs));
+
+    let target = d.getElementById(`fav${nombre}`);
+    if(target) {
+        target.innerHTML = defineStyle(nombre);
+    }
+}
+
 const instanciarDrivers = (scuderia) =>{
     let toTrash = d.querySelectorAll(`.card`);
     for(let c of toTrash){
             c.remove();
-        }
+    }
 
-     for(let dr of drivers){
-                let div = d.createElement('div');
-                div.id = dr.getNum;
-                div.className = `card ${dr.scuderia}`;
-                div.style.setProperty(`--main-color`,` #${dr.color}`)
-                div.style.setProperty(`--secondary-color`,`hsl(from var(--main-color) calc(h + 180) 10 15)`)
-                let header = d.createElement('h2');
-                header.innerHTML = dr.name;
-                let figure = d.createElement('figure');
-                let img = d.createElement('img');
-                img.src= dr.picture;
-                let button = d.createElement('button');
-                button.innerHTML = 'Ver info';
-                button.addEventListener('click', async () => {
-                let list = d.createElement('ul');
-                let li1 = d.createElement('li');
-                const points = await getInfo(dr.getNum, 'points_current');
-                console.log(points);
-                switch(points){
-                    case "null":
-                        points = '0';
-                        break;
-                    default:
-                        break;
-                }
-                li1.innerHTML = `Puntos: ${points}`;
-                let li2 = d.createElement('li');
-                    const pos = await getInfo(dr.getNum, 'position_current');
-                    li2.innerHTML = `Posición en tabla: ${pos}`;
+    for(let dr of drivers){
+               let div = d.createElement('div');
+               div.id = dr.getNum;
+               div.className = `card ${dr.scuderia}`;
+               div.style.setProperty(`--main-color`,` #${dr.color}`)
+               div.style.setProperty(`--secondary-color`,`hsl(from var(--main-color) calc(h + 180) 10 15)`)
 
-                    list.appendChild(li1);
-                    list.appendChild(li2);
-                    div.appendChild(list);
-                })
+               let header = d.createElement('h2');
+               header.innerHTML = dr.name;
 
-                figure.appendChild(img);
-                div.appendChild(header);
-                div.appendChild(figure);
-                div.appendChild(button);
-            
-            
-            
+               let figure = d.createElement('figure');
+               let img = d.createElement('img');
+               img.src= dr.picture;
+
+               let fav = d.createElement('button');
+               fav.classList = 'fav';
+               fav.id = `fav${dr.name}`;
+               fav.innerHTML = defineStyle(dr.name);
+                
+               fav.addEventListener('click', () =>{
+                    gestionarFavoritos(dr.name)
+                });
+
+               let button = d.createElement('button');
+               button.innerHTML = 'Ver info';
+               button.addEventListener('click', async () => {
+               let list = d.createElement('ul');
+               let li1 = d.createElement('li');
+               const points = await getInfo(dr.getNum, 'points_current');
+               console.log(points);
+               switch(points){
+                   case "null":
+                       points = '0';
+                       break;
+                   default:
+                       break;
+               }
+               li1.innerHTML = `Puntos: ${points}`;
+               let li2 = d.createElement('li');
+                   const pos = await getInfo(dr.getNum, 'position_current');
+                   li2.innerHTML = `Posición en tabla: ${pos}`;
+
+                   list.appendChild(li1);
+                   list.appendChild(li2);
+                   div.appendChild(list);
+            })
+
+            figure.appendChild(img);
+            div.appendChild(fav);
+            div.appendChild(header);
+            div.appendChild(figure);
+            div.appendChild(button);
             container.appendChild(div);
         }
 }
